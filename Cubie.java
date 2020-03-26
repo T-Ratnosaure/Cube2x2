78c2fc8972723a53b791a03ccc84428dc4dc7731
Aydin-ab <62435714+Aydin-ab@users.noreply.github.com>
2020-03-26 17:37:17 +0100

    Update Cubie.java

--- Cubie.java
+++ Cubie.java
@@ -2,9 +2,10 @@ import java.util.List;
 
 /**
  *
- * Classe représentant un Cubie, c'est à dire un angle du cube
+ * Classe reprÃ©sentant un Cubie, c'est Ã  dire un angle du cube
  *
- * @author Léandre Adam
+ * @author LÃ©andre Adam
+ * @author Aydin Abiar
  *
  */
 public class Cubie {
@@ -13,25 +14,28 @@ public class Cubie {
 	 * Les 3 couleurs du Cubie
 	 */
 	private List<String> colors;
+	
 	/**
-	 * Position du Cubie dans le Cube résolu
+	 * Position du Cubie dans le Cube rÃ©solu
 	 */
 	@SuppressWarnings("unused")
 	private int positionResolue;
+	
 	/**
-	 * Position du Cubie dans le Cube mélangé
+	 * Position du Cubie dans le Cube mÃ©langÃ©
 	 */
 	private int positionActuelle;
+	
 	/**
-	 * Orientation du Cubie dans le Cube mélangé
+	 * Orientation du Cubie dans le Cube mÃ©langÃ©
 	 */
 	private int orientation;
 	
 	/**
 	 * 
-	 * @param posIn la position du Cubie dans le Cube résolu
-	 * @param posAc la position du Cubie dans le Cube mélangé
-	 * @param ori l'orientation du Cubie dans le Cube mélangé
+	 * @param posIn la position du Cubie dans le Cube rÃ©solu
+	 * @param posAc la position du Cubie dans le Cube mÃ©langÃ©
+	 * @param ori l'orientation du Cubie dans le Cube mÃ©langÃ©
 	 */
 	public Cubie(int posRes, int posAc, int ori) {
 		positionResolue=posRes;
@@ -40,15 +44,15 @@ public class Cubie {
 	}
 	
 	/**
-	 * Récupère la position du Cubie dans le Cube mélangé
-	 * @return positionActuelle : la position du Cubie dans le Cube mélangé
+	 * RÃ©cupÃ¨re la position du Cubie dans le Cube mÃ©langÃ©
+	 * @return positionActuelle : la position du Cubie dans le Cube mÃ©langÃ©
 	 */
 	public int getPos() {
 		return positionActuelle;
 	}
 	
 	/**
-	 * Récupère l'orientation du cubie.
+	 * RÃ©cupÃ¨re l'orientation du cubie.
 	 * @return orientation : l'orientation du cubie
 	 */
 	public int getOr() {
@@ -56,7 +60,7 @@ public class Cubie {
 	}
 	
 	/**
-	 * Change la position du Cubie dans le Cube mélangé
+	 * Change la position du Cubie dans le Cube mÃ©langÃ©
 	 * @param p : nouvelle position du Cubie
 	 */
 	public void setPos(int posAc) {
@@ -64,7 +68,7 @@ public class Cubie {
 	}
 	
 	/**
-	 * Change l'orientation du Cubie dans le Cube mélangé
+	 * Change l'orientation du Cubie dans le Cube mÃ©langÃ©
 	 * @param p : nouvelle orientation du Cubie
 	 */
 	public void setOr(int oriAc) {
